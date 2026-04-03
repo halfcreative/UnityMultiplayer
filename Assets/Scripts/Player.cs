374b39b7325a276f1f45f348f76e8324e91609a6
Alan Sato <halfcreative@gmail.com>
2026-04-02 21:59:20 -0500

    Only the owner of the player can move the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -65,6 +65,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (!IsOwner) return;
         HandleMovement();
         HandleInteractions();
     }
